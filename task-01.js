9f7cabf73929c41e9cdce367664cfe833adbb340
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-21 22:09:45 +0600

    added task-01.js file

---
+++ task-01.js
@@ -0,0 +1,8 @@
+/* Task-01 */
+function multiplyNumbers(num1, num2, num3, num4) {
+    const multiply = num1 * num2 * num3 * num4;
+    return multiply;
+}
+
+const result = multiplyNumbers(19, 45, 63, 81);
+console.log('Result:', result);
